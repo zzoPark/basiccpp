8bc0ccdec7948c2b2e575871ed641c878d9213fd
zzoPark <rweee408@gmail.com>
2018-12-19 01:15:12 +0000

    문제번호 변경

--- question01-3-1.cpp
+++ question01-3-1.cpp
@@ -9,7 +9,6 @@ int main()
     std::cout << "[3, 3, 3] : " << BoxVolume(3, 3, 3) << std::endl;
     std::cout << "[5, 5, D] : " << BoxVolume(5, 5) << std::endl;
     std::cout << "[7, D, D] : " << BoxVolume(7) << std::endl;
-//    std::cout << "[D, D, D] : " << BoxVolume() << std::endl;
 }
 
 int BoxVolume(int length, int width, int height)
